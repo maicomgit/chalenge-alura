a7672675d753c13d1cb450ca79d1c5fe591fd64a
maicomgit <maicom.mateus@yahoo.com.br>
2022-10-13 17:45:43 -0300

    ajustando alguns detalhes

---
+++ script.js
@@ -0,0 +1,8 @@
+// let btn = document.querySelector('button');
+// let body = document.querySelector('body');
+
+// function modoEscuro(){
+//      body.classList.toggle('modoEscuro');
+// }
+
+// btn.onclick = modoEscuro;
